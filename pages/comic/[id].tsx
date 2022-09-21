062eb80b0bc74f2c98ef0b420495c82fd5f39910
Alexis Guzman <alexis12989x@gmail.com>
2022-09-21 17:25:50 -0500

    feat(pages): create comic page component

---
+++ pages/comic/[id].tsx
@@ -0,0 +1,11 @@
+import { Container, Text } from '@nextui-org/react';
+
+const Comic = () => {
+  return (
+    <Container>
+      <Text>Comic Page Component</Text>
+    </Container>
+  );
+};
+
+export default Comic;
