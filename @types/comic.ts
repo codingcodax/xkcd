660566f474562e5679a91a18545aa6eb8c4dfe3c
Alexis Guzman <alexis12989x@gmail.com>
2022-09-21 13:03:39 -0500

    feat(types): create comic type

---
+++ @types/comic.ts
@@ -0,0 +1,15 @@
+type Comic = {
+  id: number;
+  img: string;
+  height: number;
+  width: number;
+  month: string;
+  link: string;
+  year: string;
+  safeTitle: string;
+  alt: string;
+  title: string;
+  day: string;
+};
+
+export default Comic;
