6e113973d8d5ba844ee91faeec5e22765109fc7e
Alexis Guzman <alexis12989x@gmail.com>
2022-09-21 13:05:45 -0500

    feat(molecules): create comic card component

---
+++ components/molecules/ComicCard.tsx
@@ -0,0 +1,11 @@
+import { Container, Text } from '@nextui-org/react';
+
+const ComicCard = () => {
+  return (
+    <Container>
+      <Text>Comic Card Component</Text>
+    </Container>
+  );
+};
+
+export default ComicCard;
