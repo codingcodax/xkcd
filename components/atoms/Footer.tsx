0cf6277548a32a17285613eb69e061684eb8cf8b
Alexis Guzman <alexis12989x@gmail.com>
2022-09-22 17:37:28 -0500

    feat(atoms): create footer component

---
+++ components/atoms/Footer.tsx
@@ -0,0 +1,11 @@
+import { Container, Text } from '@nextui-org/react';
+
+const Footer = () => {
+  return (
+    <Container>
+      <Text>Footer Component</Text>
+    </Container>
+  );
+};
+
+export default Footer;
