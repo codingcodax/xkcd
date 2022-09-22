37cf0d0af9ea345591f7bb83215fdf5d35c79502
Alexis Guzman <alexis12989x@gmail.com>
2022-09-22 09:12:16 -0500

    feat(app): import header component outside of container component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import type { AppProps } from 'next/app';
 import { Container, NextUIProvider } from '@nextui-org/react';
 
+import { Header } from '~/components/organisms';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <NextUIProvider>
+      <Header />
       <Container xs>
         <Component {...pageProps} />
       </Container>
