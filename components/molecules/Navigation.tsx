a42d254838d34341bfb8386d9d293c8c3289bd0d
Alexis Guzman <alexis12989x@gmail.com>
2022-09-22 17:29:27 -0500

    feat(molecules): setup navigation component

--- components/molecules/Navigation.tsx
+++ components/molecules/Navigation.tsx
@@ -1,10 +1,38 @@
-import { Container, Text } from '@nextui-org/react';
+import NextLink from 'next/link';
+import { Link, Row } from '@nextui-org/react';
+import { FC } from 'react';
 
-const Navigation = () => {
+interface NavigationProps {
+  hasPrevios: boolean;
+  hasNext: boolean;
+  prevId: number;
+  nextId: number;
+}
+
+const Navigation: FC<NavigationProps> = ({
+  hasPrevios,
+  hasNext,
+  prevId,
+  nextId,
+}) => {
   return (
-    <Container>
-      <Text>Navigation Component</Text>
-    </Container>
+    <Row justify='space-between'>
+      {hasPrevios ? (
+        <NextLink href={`/comic/${prevId}`}>
+          <Link>&larr; Previous</Link>
+        </NextLink>
+      ) : (
+        <div></div>
+      )}
+
+      {hasNext ? (
+        <NextLink href={`/comic/${nextId}`}>
+          <Link>Next &rarr;</Link>
+        </NextLink>
+      ) : (
+        <div></div>
+      )}
+    </Row>
   );
 };
 
