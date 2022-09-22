8860e34cca303aabda1b79e77db0e4b22db4b88b
Alexis Guzman <alexis12989x@gmail.com>
2022-09-22 16:59:01 -0500

    feat(molecules): create navigation component

---
+++ components/molecules/Navigation.tsx
@@ -0,0 +1,11 @@
+import { Container, Text } from '@nextui-org/react';
+
+const Navigation = () => {
+  return (
+    <Container>
+      <Text>Navigation Component</Text>
+    </Container>
+  );
+};
+
+export default Navigation;
