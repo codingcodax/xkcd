008c39478f0c2b32caee0a4de5e890dc4d1a3235
Alexis Guzman <alexis12989x@gmail.com>
2022-09-23 14:10:52 -0500

    feat(atoms): setup footer component

--- components/atoms/Footer.tsx
+++ components/atoms/Footer.tsx
@@ -1,9 +1,12 @@
-import { Container, Text } from '@nextui-org/react';
+import { Container } from '@nextui-org/react';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <Container>
-      <Text>Footer Component</Text>
+    <Container css={{ mt: '$12', textAlign: 'center', fontWeight: 'bold' }}>
+      <Link href='https://xkcd.com/' rel='noopener noreferrer' target='_blank'>
+        <a>all comics by xkcd</a>
+      </Link>
     </Container>
   );
 };
