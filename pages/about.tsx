d5ed6669082101c0cfc3b7a6a2e130be356f5c9f
Alexis Guzman <alexis12989x@gmail.com>
2022-09-26 13:18:15 -0500

    feat(pages): setup about page component

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,32 @@
-import { Text } from '@nextui-org/react';
+import NextLink from 'next/link';
+import { Link, Text } from '@nextui-org/react';
 
 const About = () => {
   return (
     <div>
-      <Text>About Page Componen</Text>
+      <Text>
+        About from{' '}
+        <NextLink passHref href='https://xkcd.com/'>
+          <Link
+            css={{ d: 'inline-block' }}
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            xkcd.com
+          </Link>
+        </NextLink>
+      </Text>
+
+      <Text h2>Who are you?</Text>
+      <Text>
+        I&apos;m just this guy, you know? I&apos;m a CNU graduate with a degree
+        in physics. Before starting xkcd, I worked on robots at NASA&apos;s
+        Langley Research Center in Virginia. As of June 2007 I live in
+        Massachusetts. In my spare time I climb things, open strange doors, and
+        go to goth clubs dressed as a frat guy so I can stand around and look
+        terribly uncomfortable. At frat parties I do the same thing, but the
+        other way around.
+      </Text>
     </div>
   );
 };
