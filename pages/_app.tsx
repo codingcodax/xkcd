cc6170e2b517359cc030731fec69feafef64736f
Alexis Guzman <alexis12989x@gmail.com>
2022-09-26 14:06:55 -0500

    feat(app): import default seo and next seo config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import type { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
 import { Container, NextUIProvider } from '@nextui-org/react';
 
 import { Header } from '~/components/organisms';
 import { Footer } from '~/components/atoms';
+import SEO from '~/next-seo.config';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <NextUIProvider>
       <Header />
       <Container sm>
+        <DefaultSeo {...SEO} />
         <Component {...pageProps} />
       </Container>
       <Footer />
